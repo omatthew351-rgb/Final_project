5415627446dcf426c673cd22c5554d41219bd5c6
Son Thuan Do <sonjake@Sons-MacBook-Pro.local>
2025-11-03 11:28:54 -0500

    added template

---
+++ main.py
@@ -0,0 +1,47 @@
+import sys
+
+import pygame
+import pygame.locals
+import random
+
+
+WIDTH, HEIGHT = 640, 480
+
+
+class Ball:
+
+    def __init__(self, x: float, y: float):
+        self.x = x
+        self.y = y
+        self.vx = random.uniform(-5, 5)
+        self.vy = random.uniform(-5, 5)
+
+    def update(self, surface: pygame.Surface) -> None:
+        self.x += self.vx
+        self.y += self.vy
+        pygame.draw.circle(surface, "#ff0000", (self.x, self.y), 30)
+
+
+def main():
+    fps = 60
+    fps_clock = pygame.time.Clock()
+    pygame.init()
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+    ball = Ball(WIDTH/2, HEIGHT/2)
+
+    while True:
+        screen.fill("#000000")
+        ball.update(screen)
+
+        for event in pygame.event.get():
+            if event.type == pygame.locals.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        pygame.display.flip()
+        fps_clock.tick(fps)
+
+
+if __name__ == "__main__":
+    main()
