b10eefd04e5118f6f652746906e28ef515dc999a
Son Do <sonjake2020@gmail.com>
2025-11-04 12:43:10 -0500

    add player and enemy class

--- main.py
+++ main.py
@@ -8,7 +8,20 @@ import random
 WIDTH, HEIGHT = 640, 480
 
 
-class Ball:
+class Player:
+
+    def __init__(self, x: float, y: float):
+        self.x = x
+        self.y = y
+        self.vx = random.uniform(-5, 5)
+        self.vy = random.uniform(-5, 5)
+
+    def update(self, surface: pygame.Surface) -> None:
+        self.x += self.vx
+        self.y += self.vy
+        pygame.draw.circle(surface, "#ff0000", (self.x, self.y), 30)
+
+class Enemy:
 
     def __init__(self, x: float, y: float):
         self.x = x
@@ -26,18 +39,17 @@ def main():
     fps = 60
     fps_clock = pygame.time.Clock()
     pygame.init()
-    screen = pygame.display.set_mode((WIDTH, HEIGHT))
-
-    ball = Ball(WIDTH/2, HEIGHT/2)
-
+    screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.FULLSCREEN)
     while True:
         screen.fill("#000000")
-        ball.update(screen)
-
         for event in pygame.event.get():
             if event.type == pygame.locals.QUIT:
                 pygame.quit()
                 sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
 
         pygame.display.flip()
         fps_clock.tick(fps)
